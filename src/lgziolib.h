63b512f6706e951f8d5a4f9bc9cb29aae38b6723
Judge Maygarden <github.jmaygarden@safersignup.com>
2011-12-27 09:55:26 -0600

    Added LuaJIT compatability with a patch contributed by Denis Usenko

--- src/lgziolib.h
+++ src/lgziolib.h
@@ -15,6 +15,15 @@
 #define GZIO_API	LUA_API
 #endif /* GZIO_API */
 
+/*
+ * The macros "lua_popen" and "lua_pclose" are defined in luaconf.h. In JIT
+ * version "lua_open" and "lua_close" do not exists.
+ */
+#ifndef lua_popen
+#   define lua_popen(L, c, m)   ((void)(L), _popen((c),(m)))
+#   define lua_pclose(L, file)  ((void)(L), (_pclose((file)) != -1))
+#endif
+
 #define LUA_GZIOLIBNAME   "gzio"
 GZIO_API int luaopen_gzio (lua_State *L);
 
